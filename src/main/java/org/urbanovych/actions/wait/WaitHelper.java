6ed1cbe08dd575fdf1d30f16dbe98d70bcb8e7eb
Urbanovych <urbanovych.qa@gmail.com>
2023-10-09 14:54:11 +0300

    YU: - Remove extends from WaitHelper.java

--- src/main/java/org/urbanovych/actions/wait/WaitHelper.java
+++ src/main/java/org/urbanovych/actions/wait/WaitHelper.java
@@ -3,22 +3,23 @@ package org.urbanovych.actions.wait;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.urbanovych.BaseTest;
 
-public class WaitHelper extends BaseTest {
+import static org.urbanovych.BaseTest.getWait;
+
+public class WaitHelper {
 
     private static final String layoutTitleId = "android:id/action_bar_title";
 
     public static WebElement getVisibleElementByXpath(String xpathLocator) {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpathLocator)));
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpathLocator)));
     }
 
     public static WebElement getVisibleElementById(String idLocator) {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(idLocator)));
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(By.id(idLocator)));
     }
 
     public static void waitForPageLoaded() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(layoutTitleId)));
+        getWait().until(ExpectedConditions.visibilityOfElementLocated(By.id(layoutTitleId)));
     }
 
 }
